1a093eafc9d21af10bc6a0f2fe80ca6b9aa3d559
Rupam <rupammondal564@gmail.com>
2025-02-20 09:45:32 +0530

    Update in home ui

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,13 @@ import Auth from "./pages/Auth";
 import Dashboard from "./pages/Dashboard";
 import Home from "./pages/Home";
 import { Routes, Route } from "react-router-dom";
+import Home1 from "./pages/Home1";
 
 function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home1 />} />
         <Route path="/auth" element={<Auth />} />
         <Route path="/dashboard/:userid" element={<Dashboard />} />
       </Routes>
---
+++ src/pages/Home1.jsx
@@ -0,0 +1,13 @@
+import ParticlesDemo from "@/components/ParticleBackground/Particlebg";
+
+function Home1(){
+    return (
+        <>
+            <div className="h-[100vh] w-full">
+                <ParticlesDemo/>
+            </div>
+        </>
+    )
+}
+
+export default Home1;
