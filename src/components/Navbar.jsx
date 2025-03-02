7dc5ce57a3fb0d8edbe04779d337e753c8fa1da1
Rupam <rupammondal564@gmail.com>
2025-03-02 12:06:22 +0530

    Profile pic added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,8 @@ const Navbar = () => {
 
   const navigate = useNavigate();
 
-  const { auth,setAuth } = useContext(UserContext);
+  const { auth, setAuth } = useContext(UserContext);
+  const user = JSON.parse(localStorage.getItem("user"));
 
   return (
     <div className="fixed z-10 top-0 right-0 w-full bg-black/30 backdrop-blur-lg shadow-lg px-5 py-4 flex justify-between items-center">
@@ -14,27 +15,33 @@ const Navbar = () => {
       <div className="text-xl font-bold text-white">Nav.</div>
 
       {/* Buttons */}
-      <div className="flex gap-4 sm:gap-8">
-        <button
-          className="text-white border border-white px-4 py-2 rounded-md transition duration-300 hover:bg-white hover:text-black hover:shadow-lg"
-          onClick={() => {
-            setAuth("login");
-            navigate("/auth");
-          }}
-        >
-          Login
-        </button>
-        <button
-          className="bg-white text-black px-4 py-2 rounded-md transition duration-300 hover:bg-gray-500 hover:shadow-lg"
-          onClick={() => {
-            setAuth("signup");
-            navigate("/auth");
-          }}
-        >
-          Sign up
-        </button>
-      </div>
-    </div>
+      {
+        user ? (
+          <div className="h-12 w-12 cursor-pointer"><img className="h-full w-full" src={ user?.avatar} alt="" /></div>
+        ) : (
+          <div className="flex gap-4 sm:gap-8">
+            <button
+              className="text-white border border-white px-4 py-2 rounded-md transition duration-300 hover:bg-white hover:text-black hover:shadow-lg"
+              onClick={() => {
+                setAuth("login");
+                navigate("/auth");
+              }}
+            >
+              Login
+            </button>
+            <button
+              className="bg-white text-black px-4 py-2 rounded-md transition duration-300 hover:bg-gray-500 hover:shadow-lg"
+              onClick={() => {
+                setAuth("signup");
+                navigate("/auth");
+              }}
+            >
+              Sign up
+            </button>
+          </div >
+        )
+      }
+    </div >
   );
 };
 
