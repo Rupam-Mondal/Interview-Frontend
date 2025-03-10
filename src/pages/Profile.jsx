c8cdb5d7317aae0855756786677d998a044c2af0
Sandipan <sandipanseth2004@gmail.com>
2025-03-10 12:25:33 +0530

    changing profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -6,22 +6,33 @@ import React from "react";
 
 const Profile = () => {
   const user = JSON.parse(localStorage.getItem("user"));
-  
 
   return (
-    <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center relative">
+    <div className="bg-black text-white min-h-screen flex flex-col items-center justify-center relative px-6 ">
+      {/* Background Particles */}
       <Particles
-        className="absolute inset-0"
-        quantity={200}
+        className="absolute inset-0 h-full"
+        quantity={150}
         ease={80}
         color="#ffffff"
         refresh
       />
-      <div className="fixed top-5 left-5 border border-white rounded-full p-3 hover:bg-white hover:text-black transition-all duration-200 cursor-pointer">
-        <ArrowLeft className="w-6 h-6" onClick={() => window.history.back()} />
+
+      {/* Back Button */}
+      <button
+        className="fixed top-5 left-5 border border-white rounded-full p-3 hover:bg-white hover:text-black transition-all duration-200"
+        onClick={() => window.history.back()}
+      >
+        <ArrowLeft className="w-6 h-6" />
+      </button>
+
+      {/* User Profile Content */}
+      <div className="z-10 w-full max-w-4xl flex flex-col items-center gap-6">
+        <div className="overflow-hidden w-full">
+          {/* <Content user={user} /> */}
+          <Card user={user} />
+        </div>
       </div>
-      <Card user={user} />
-      <Content />
     </div>
   );
 };
